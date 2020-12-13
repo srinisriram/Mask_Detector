e708090b8521a70996296908f1a8b45c19240777
srinisriram <srinivasssriram06@gmail.com>
2020-12-13 11:42:21 -0500

    First commit - make ALL Code changes on github

---
+++ Mask_Detector/mask_detector.py
@@ -0,0 +1,101 @@
+# Import necessary packages
+import threading
+import time
+
+import cv2
+from detect import detect
+from play_audioMask import PlayAudio
+from tensorflow.keras.models import load_model
+from vars import prototxt_path, face_model_path, mask_model_path, min_mask_confidence, video_cam_index
+
+# Load all the models, and start the camera stream
+faceModel = cv2.dnn.readNet(prototxt_path, face_model_path)
+faceModel.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
+maskModel = load_model(mask_model_path)
+stream = cv2.VideoCapture(video_cam_index)
+
+AudioPlay = False
+playAudio = False
+
+
+def thread_for_when_to_play_audio():
+    """
+    This function is used for playing the alarm if a person is not wearing a mask.
+    :return:
+    """
+    global playAudio
+    while True:
+        if playAudio:
+            play_audio()
+
+
+def play_audio():
+    """
+    This function is used for playing the alarm if a person is not wearing a mask.
+    :return:
+    """
+    global AudioPlay
+    global playAudio
+    SoundThread = threading.Thread(target=PlayAudio.play_audio_file)
+    print("[INFO]: Starting Sound Thread")
+    if not AudioPlay:
+        AudioPlay = True
+        SoundThread.start()
+        time.sleep(3)
+        AudioPlay = False
+        playAudio = False
+        print("[INFO]: Stopping Sound Thread")
+
+
+def thread_for_mask_detection():
+    global faceModel
+    global maskModel
+    global stream
+    global playAudio
+    while True:
+        # Read frame from the stream
+        ret, frame = stream.read()
+
+        # Run the detect function on the frame
+        (locations, predictions) = detect(frame, faceModel, maskModel)
+
+        # Go through each face detection.
+        for (box, pred) in zip(locations, predictions):
+            # Extract the prediction and bounding box coords
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
+
+            confidence = max(mask, withoutMask) * 100
+            # Determine the class label and make actions accordingly
+            if mask >= withoutMask:
+                if confidence > min_mask_confidence:
+                    label = 'Mask ' + str(confidence)
+                    print(label)
+                    color = (0, 255, 0)
+            else:
+                if confidence > min_mask_confidence:
+                    label = 'No Mask ' + str(confidence)
+                    print(label)
+                    playAudio = True
+                    color = (0, 0, 255)
+
+            # Place label and Bounding Box
+            #cv2.putText(frame, label, (startX, startY - 10),
+            #            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            #cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # break from loop if key pressed is q
+        if key == ord("q"):
+            break
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=thread_for_when_to_play_audio)
+
+    t1.start()
+
+    thread_for_mask_detection()
---
+++ Mask_Detector/vars.py
@@ -0,0 +1,14 @@
+prototxt_path = "models/deploy.prototxt"
+
+face_model_path = "models/res10_300x300_ssd_iter_140000.caffemodel"
+
+mask_model_path = "models/very_very_good_mask_detector.h5"
+
+video_cam_index = 0
+
+label = ""
+
+min_mask_confidence = 98
+
+sound_file = "speech1.wav"
+
